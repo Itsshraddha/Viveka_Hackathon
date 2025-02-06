65a16e7c73477f0532addcbcde2231e434e3d6ff
Itsshraddha <shraddhabajpai90@gmail.com>
2025-02-06 21:47:14 +0530

    first

---
+++ website/script.js
@@ -0,0 +1,8 @@
+let a= document.getElementById("sec")
+function changeColor(){
+    a.style.backgroundColor="green"
+}
+let b=document.getElementById("don")
+function changeColor1(){
+    b.style.backgroundColor="lightgreen"
+}
